c04eeb61289e43781f300f390a8b87b0b90dd30e
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2022-10-11 16:06:05 -0400

    Added session test

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "tis a secret"
 
 @app.route('/')
 def index():
@@ -9,8 +10,15 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print ("Got Post Info")
+    session["username"] = request.form['name']
+    session["useremail"] = request.form['email']
+    return redirect('/show')
+
+@app.route("/show")
+def show_user():
+    print ("Showing the User Info from the form")
     print (request.form)
-    return redirect('/')
+    return render_template("show.html", name_on_template=session["username"], email_on_template=session["useremail"])
 
 if __name__ == "__main__":
     app.run(port=8000 ,debug = True)
